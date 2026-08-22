b3aac2619c38d3d9a7a4f24ec4ce6fb27d3b6d28
Guofeng Luo <luoguofeng0401@gmail.com>
2026-08-23 02:44:29 +0800

    4-1

--- AllDoneCompleteCourse/AllDoneCompleteCourseApp.swift
+++ AllDoneCompleteCourse/AllDoneCompleteCourseApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AllDoneCompleteCourseApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             AppStortingView()
         }
     }
 }
+
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
