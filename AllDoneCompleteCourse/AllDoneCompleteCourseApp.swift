3dbc427ad157701005561e9fc101c62e34e04931
Guofeng Luo <luoguofeng0401@gmail.com>
2026-08-23 01:52:01 +0800

    Initial Commit

---
+++ AllDoneCompleteCourse/AllDoneCompleteCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneCompleteCourseApp.swift
+//  AllDoneCompleteCourse
+//
+//  Created by Guofeng Luo on 2026/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneCompleteCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
